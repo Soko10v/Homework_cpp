deb9efeeffa5b4d1cd10817e7043b5947efeca99
Soko10v <Sokolovp2001@mail.ru>
2021-05-17 23:56:39 +0300

    выполнил дз с 1 по 4 пункты

---
+++ hw2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+    //1
+    int integer = 1;
+    float num = 1;
+    bool Y_or_N = 0;
+    char name = 74;
+    short small = 2;
+    long big = 22222;
+    long long very_big = 222222222;
+    std::cout <<  "созднны переменные" << std::endl;
+    
+    //2
+    enum TicTakToe{empty, X, O};
+    
+    //3
+    int TicTakArr [9]={1,0,0,2,1,2,2,1,2};
+    
+    //4
+    struct field
+    {
+        int a;
+        int b;
+        //нужно снабдить массивом TicTakarr но он уже был создан и инициализирован в задании 3
+    };
+    //5
+    
+    
+    return 0;
+}
